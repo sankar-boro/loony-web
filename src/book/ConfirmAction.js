432c353adc2dd1d94af92824ea1435d8196d00ad
Sankar Boro <sankar.boro@yahoo.com>
2024-03-28 10:40:20 +0530

    confirmAction

---
+++ src/book/ConfirmAction.js
@@ -0,0 +1,14 @@
+import { Modal, ModalBodyContainer, ModalButtonContainer } from '../components';
+
+const ConfirmAction = ({ onClose, title, confirmTitle, confirmAction }) => {
+  return (
+    <Modal visible={true} onClose={onClose} title={title}>
+      <ModalBodyContainer>{confirmTitle}</ModalBodyContainer>
+      <ModalButtonContainer>
+        <button onClick={confirmAction}>Confirm</button>
+      </ModalButtonContainer>
+    </Modal>
+  );
+};
+
+export default ConfirmAction;
