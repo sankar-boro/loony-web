151b770f274ce90e2f0bf6fd5c008d721bd17ee1
Sankar Boro <sankar.boro@yahoo.com>
2024-05-15 09:56:20 +0530

    Remove loader on data fetch complete

--- src/Home.js
+++ src/Home.js
@@ -6,8 +6,8 @@ import CardLoader from './components/CardLoader';
 const Home = () => {
   const navigate = useNavigate();
 
-  const [blogs, setBlogs] = useState([]);
-  const [books, setBooks] = useState([]);
+  const [blogs, setBlogs] = useState(null);
+  const [books, setBooks] = useState(null);
 
   useEffect(() => {
     axiosInstance.get('/blog/get_all_blogs').then(({ data }) => {
@@ -23,90 +23,92 @@ const Home = () => {
       <div className='app-body'>
         <h3>Blogs</h3>
         <div className='flex-row' style={{ flexWrap: 'wrap' }}>
-          {blogs.length === 0
+          {!blogs
             ? [1, 2, 3, 4].map((key_id) => {
                 return <CardLoader key_id={key_id} />;
               })
             : null}
-          {blogs.map((blog) => {
-            const images = blog.images ? JSON.parse(blog.images) : [];
-            const image = images.length > 0 ? images[0] : null;
-            return (
-              <div className='card' key={blog.blog_id}>
-                <div
-                  className='card-image'
-                  style={{
-                    backgroundImage:
-                      image && image.name
-                        ? `url("${process.env.REACT_APP_BASE_API_URL}/api/i/${image.name}")`
-                        : null,
-                    overflow: 'hidden',
-                    backgroundSize: 'cover',
-                    backgroundPosition: 'center',
-                    borderTopLeftRadius: 3,
-                    borderTopRightRadius: 3,
-                  }}
-                  onClick={() => {
-                    navigate(`/view/blog/${blog.blog_id}`, blog);
-                  }}
-                />
-                <div className='card-body'>
+          {blogs &&
+            blogs.map((blog) => {
+              const images = blog.images ? JSON.parse(blog.images) : [];
+              const image = images.length > 0 ? images[0] : null;
+              return (
+                <div className='card' key={blog.blog_id}>
                   <div
-                    className='card-title cursor'
+                    className='card-image'
+                    style={{
+                      backgroundImage:
+                        image && image.name
+                          ? `url("${process.env.REACT_APP_BASE_API_URL}/api/i/${image.name}")`
+                          : null,
+                      overflow: 'hidden',
+                      backgroundSize: 'cover',
+                      backgroundPosition: 'center',
+                      borderTopLeftRadius: 3,
+                      borderTopRightRadius: 3,
+                    }}
                     onClick={() => {
                       navigate(`/view/blog/${blog.blog_id}`, blog);
                     }}
-                  >
-                    {blog.title}
+                  />
+                  <div className='card-body'>
+                    <div
+                      className='card-title cursor'
+                      onClick={() => {
+                        navigate(`/view/blog/${blog.blog_id}`, blog);
+                      }}
+                    >
+                      {blog.title}
+                    </div>
+                    <div className='card-body' />
                   </div>
-                  <div className='card-body' />
                 </div>
-              </div>
-            );
-          })}
+              );
+            })}
         </div>
         <h3>Books</h3>
         <div className='flex-row' style={{ flexWrap: 'wrap' }}>
-          {books.length === 0
+          {!books
             ? [1, 2, 3, 4].map((key_id) => {
                 return <CardLoader key_id={key_id} />;
               })
             : null}
-          {books.map((book) => {
-            const image = JSON.parse(book.images)[0];
-            return (
-              <div className='card' key={book.book_id}>
-                <div
-                  className='card-image'
-                  style={{
-                    backgroundImage:
-                      image && image.name
-                        ? `url("${process.env.REACT_APP_BASE_API_URL}/api/i/${image.name}")`
-                        : null,
-                    overflow: 'hidden',
-                    backgroundSize: 'cover',
-                    backgroundPosition: 'center',
-                    borderTopLeftRadius: 3,
-                    borderTopRightRadius: 3,
-                  }}
-                  onClick={() => {
-                    navigate(`/view/book/${book.book_id}`, book);
-                  }}
-                />
-                <div className='card-body'>
+          {books &&
+            books.map((book) => {
+              const image = JSON.parse(book.images)[0];
+              return (
+                <div className='card' key={book.book_id}>
                   <div
-                    className='card-title cursor'
+                    className='card-image'
+                    style={{
+                      backgroundImage:
+                        image && image.name
+                          ? `url("${process.env.REACT_APP_BASE_API_URL}/api/i/${image.name}")`
+                          : null,
+                      overflow: 'hidden',
+                      backgroundSize: 'cover',
+                      backgroundPosition: 'center',
+                      borderTopLeftRadius: 3,
+                      borderTopRightRadius: 3,
+                    }}
                     onClick={() => {
                       navigate(`/view/book/${book.book_id}`, book);
                     }}
-                  >
-                    {book.title}
+                  />
+                  <div className='card-body'>
+                    <div
+                      className='card-title cursor'
+                      onClick={() => {
+                        navigate(`/view/book/${book.book_id}`, book);
+                      }}
+                    >
+                      {book.title}
+                    </div>
+                    <div className='card-body' />
                   </div>
-                  <div className='card-body' />
                 </div>
-              </div>
-            );
-          })}
+              );
+            })}
         </div>
       </div>
       <div style={{ height: 50 }} />
